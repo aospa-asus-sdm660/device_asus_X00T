ba3e68b78eee7e05e4c29a977b99fbc272fd9c06
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-08-06 23:49:32 +0300

    X00T: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    https://github.com/PixelExp-Project/vendor_aosp/commit/c3b9f5c170c794966bb941282c3f9f18baa6279a
    
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, vendor/asus/X00T/X00T-vendor.mk)
 # APEX
 PRODUCT_COMPRESSED_APEX := false
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 # Default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
