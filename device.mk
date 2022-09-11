c68a5f5a1e29fe0394a060663eb6a7b56b96feac
karthik <karthik1896@outlook.com>
2022-09-11 14:57:53 +0530

    X00T:Update Bluetooth - from LA.UM.9.6.2.r1-03600-89xx.0
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -84,7 +84,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # QTI Bluetooth
 include vendor/qcom/opensource/commonsys-intf/bluetooth/bt-commonsys-intf-board.mk
