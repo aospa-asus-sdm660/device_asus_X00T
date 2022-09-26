bdb9ae25497db435f653e4759bcf3a07b1e3de14
karthik1896 <karthik1896@outlook.com>
2022-09-26 20:37:02 +0530

    X00T:do not include device/qcom/sepolicy
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,8 +153,6 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
