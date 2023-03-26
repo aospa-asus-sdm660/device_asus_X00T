ffc0a25ea2ebc3aeaaab08d52838179bf9e9a6fe
Fabian Leuteneegger <fabian.leutenegger@bluewin.ch>
2023-03-26 08:40:47 +0530

    X00T: Build Neural Networks HIDL
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-qti": library "android.hardware.neuralnetworks@1.0.so" not found: needed by main executable
    
    Change-Id: Ic6e1ea31f01c13fc6f2fa3dadf4bee672249f295
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Neuralnetworks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_NFC/android.hardware.nfc.hce.xml \
