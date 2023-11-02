b61b1284fbefce85a22923dde3e403ce28f8a521
karthik1896 <karthik1896@outlook.com>
2023-11-02 03:20:23 +0100

    X00T: Build Permissive for now
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,7 +82,7 @@ BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 loop.max_part=7 kpti=off
 BOARD_KERNEL_CMDLINE += cgroup_disable=pressure
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_MKBOOTIMG_ARGS := --header_version 1
